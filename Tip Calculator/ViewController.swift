acab4f3407059a81ce890926cea63699f3237dc1
ahandal24 <61199977+ahandal24@users.noreply.github.com>
2021-06-10 20:30:33 -0400

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Amber Handal on 6/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
